25f4f91d2d9fdd1195c3b1df99f3b5c019a9db22
Abdulsalam001Arnold <abdulsalamabulhamidarnold@gmail.com>
2025-07-20 17:54:49 +0100

    updated

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,7 +6,3 @@
 export const API_BASE_URL = window.location.protocol === 'https:' 
   ? "https://v1-api-6rdd.onrender.com"
   : "http://localhost:3000";
-
-
-  
-  // export const API_BASE_URL = "https://v1-api-6rdd.onrender.com"
